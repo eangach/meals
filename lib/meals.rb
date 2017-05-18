bc407f7ad8433b6d590b21f26817231521167eb3
Thomas Behrndt <thomas.behrndt@me.com>
2017-05-18 15:45:58 +0930

    Pass recipe list to recipes view

--- lib/meals.rb
+++ lib/meals.rb
@@ -3,7 +3,9 @@ require 'sinatra/base'
 class Meals < Sinatra::Base
 
   get '/recipes' do
-    haml :recipes
+    @recipes =  [ 'Sample recipe 1', 'Sample recipe 2' ]
+
+    haml :recipes, locals: { recipes: @recipes }
   end
 
   run! if __FILE__ == $PROGRAM_NAME
