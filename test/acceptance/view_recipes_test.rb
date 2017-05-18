6b04a69be12e55e837618c4dd66bc6164e120368
Thomas Behrndt <thomas.behrndt@me.com>
2017-05-18 16:16:19 +0930

    Link to recipe page from recipes list

--- test/acceptance/view_recipes_test.rb
+++ test/acceptance/view_recipes_test.rb
@@ -4,8 +4,8 @@ describe "my example app" do
 
   it 'has a recipes page' do
     visit '/recipes'
-    page.must_have_content("Sample recipe 1")
-    page.must_have_content("Sample recipe 2")
+    page.must_have_link('Sample recipe 1')
+    page.must_have_link('Sample recipe 2')
   end
 end
 
